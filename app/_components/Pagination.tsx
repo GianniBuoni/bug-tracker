0ea3cb0690529f066fddb719ca3b8eaae507bd11
giannibuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-04-28 21:45:05 -0400

    Implemented pagination functionality

--- app/_components/Pagination.tsx
+++ app/_components/Pagination.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useSearchParams, useRouter } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -7,22 +9,48 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params);
+  };
+
   const pageCount = Math.ceil(itemCount / pageSize);
+  if (pageCount <= 1) return null;
   return (
     <div className="join">
-      <button className="join-item btn" disabled={currentPage === 1}>
+      <button
+        className="join-item btn"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         «
       </button>
-      <button className="join-item btn" disabled={currentPage === 1}>
+      <button
+        className="join-item btn"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         ‹
       </button>
       <button className="join-item btn">
         Page {currentPage} of {pageCount}
       </button>
-      <button className="join-item btn" disabled={currentPage === pageCount}>
+      <button
+        className="join-item btn"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         ›
       </button>
-      <button className="join-item btn" disabled={currentPage === pageCount}>
+      <button
+        className="join-item btn"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         »
       </button>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import Pagination from "./_components/Pagination";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
       <h1>Welcome!</h1>
+      <Pagination
+        itemCount={100}
+        currentPage={parseInt(searchParams.page)}
+        pageSize={5}
+      />
     </>
   );
 }
